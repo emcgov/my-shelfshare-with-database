4af0c8f73d1981783398df288919735f7b975f29
Brooke Thompson <brookethompson0341@gmail.com>
2024-12-08 16:18:55 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,182 +1,62 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
 const { MongoClient } = require("mongodb");
 
-async function run() {
-  // TODO:
-  // Replace the placeholder connection string below with your
-  // Altas cluster specifics. Be sure it includes
-  // a valid username and password! Note that in a production environment,
-  // you do not want to store your password in plain-text here.
-  const uri =
-    "mongodb+srv://shelfuser:Soccer123@cluster0.smdvj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const app = express();
+const uri = "mongodb+srv://shelfuser:Soccer123@cluster0.smdvj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri);
 
-  // The MongoClient is the object that references the connection to our
-  // datastore (Atlas, for example)
-  const client = new MongoClient(uri);
+app.use(bodyParser.json());
 
-  // The connect() method does not attempt a connection; instead it instructs
-  // the driver to connect using the settings provided when a connection
-  // is required.
-  await client.connect();
+// Connect to the database
+let usersCollection;
+client.connect().then(() => {
+  const db = client.db("final");
+  usersCollection = db.collection("users");
+  console.log("Connected to MongoDB");
+});
 
-  // Provide the name of the database and collection you want to use.
-  // If the database and/or collection do not exist, the driver and Atlas
-  // will create them automatically when you first write data.
-  const dbName = "final";
-  const collectionName = "users";
-
-  // Create references to the database and collection in order to run
-  // operations on them.
-  const database = client.db(dbName);
-  const collection = database.collection(collectionName);
-
-  /*
-   *  *** INSERT DOCUMENTS ***
-   *
-   * You can insert individual documents using collection.insert().
-   * In this example, we're going to create four documents and then
-   * insert them all in one call with collection.insertMany().
-   */
-
-  const users = [
-    {
-      username: "test1",
-      books: [
-        "corn",
-        "mayonnaise",
-        "cotija cheese",
-        "sour cream",
-        "lime",
-      ],
-      password: "1234",
-    },
-    {
-      username: "test2",
-      books: [
-        "ground beef",
-        "butter",
-        "onion",
-        "egg",
-        "bread bun",
-        "mushrooms",
-      ],
-      password: "1234",
-    },
-    {
-        username: "test3",
-      books: [
-        "potato",
-        "tomato",
-        "olive oil",
-        "onion",
-        "garlic",
-        "paprika",
-      ],
-      password: "1234",
-    },
-    {
-        username: "test4",
-      books: [
-        "rice",
-        "soy sauce",
-        "egg",
-        "onion",
-        "pea",
-        "carrot",
-        "sesame oil",
-      ],
-      password: "1234",
-    },
-  ];
-
-  try {
-    const insertManyResult = await collection.insertMany(users);
-    console.log(`${insertManyResult.insertedCount} documents successfully inserted.\n`);
-  } catch (err) {
-    console.error(`Something went wrong trying to insert the new documents: ${err}\n`);
+// Registration endpoint
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).send("Username and password are required");
   }
 
-  /*
-   * *** FIND DOCUMENTS ***
-   *
-   * Now that we have data in Atlas, we can read it. To retrieve all of
-   * the data in a collection, we call Find() with an empty filter.
-   * The Builders class is very helpful when building complex
-   * filters, and is used here to show its most basic use.
-   */
+  // Check if user already exists
+  const existingUser = await usersCollection.findOne({ username });
+  if (existingUser) {
+    return res.status(400).send("User already exists");
+  }
 
-  //const findQuery = { prepTimeInMinutes: { $lt: 45 } };
+  // Hash password and save to database
+  const hashedPassword = await bcrypt.hash(password, 10);
+  await usersCollection.insertOne({ username, password: hashedPassword });
+  res.status(201).send("User registered successfully");
+});
 
-  //try {
-  //  const cursor = await collection.find(findQuery).sort({ name: 1 });
-  //  await cursor.forEach(users => {
-  //    console.log(`${users.name} has ${users.ingredients.length} ingredients and takes ${users.prepTimeInMinutes} minutes to make.`);
-  //  });
-    // add a linebreak
-  //  console.log();
-  //} catch (err) {
-  //  console.error(`Something went wrong trying to find the documents: ${err}\n`);
-  //}
+// Login endpoint
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).send("Username and password are required");
+  }
 
-  // We can also find a single document. Let's find the first document
-  // that has the string "potato" in the ingredients list.
-  //const findOneQuery = { ingredients: "potato" };
-//
-  //try {
-  //  const findOneResult = await collection.findOne(findOneQuery);
-  //  if (findOneResult === null) {
-  //    console.log("Couldn't find any userss that contain 'potato' as an ingredient.\n");
-  //  } else {
-  //    console.log(`Found a users with 'potato' as an ingredient:\n${JSON.stringify(findOneResult)}\n`);
-  //  }
-  //} catch (err) {
-  //  console.error(`Something went wrong trying to find one document: ${err}\n`);
-  //}
-//
-  /*
-   * *** UPDATE A DOCUMENT ***
-   *
-   * You can update a single document or multiple documents in a single call.
-   *
-   * Here we update the PrepTimeInMinutes value on the document we
-   * just found.
-   */
-  //const updateDoc = { $set: { prepTimeInMinutes: 72 } };
+  // Find user in database
+  const user = await usersCollection.findOne({ username });
+  if (!user) {
+    return res.status(400).send("Invalid credentials");
+  }
 
-  // The following updateOptions document specifies that we want the *updated*
-  // document to be returned. By default, we get the document as it was *before*
-  // the update.
-  //const updateOptions = { returnOriginal: false };
-//
-  //try {
-  //  const updateResult = await collection.findOneAndUpdate(
-  //    findOneQuery,
-  //    updateDoc,
-  //    updateOptions,
-  //  );
-  //  console.log(`Here is the updated document:\n${JSON.stringify(updateResult.value)}\n`);
-  //} catch (err) {
-  //  console.error(`Something went wrong trying to update one document: ${err}\n`);
-  //}
+  // Check password
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(400).send("Invalid credentials");
+  }
 
-  /*      *** DELETE DOCUMENTS ***
-   *
-   *      As with other CRUD methods, you can delete a single document
-   *      or all documents that match a specified filter. To delete all
-   *      of the documents in a collection, pass an empty filter to
-   *      the DeleteMany() method. In this example, we'll delete two of
-   *      the userss.
-   */
+  res.status(200).send("Login successful");
+});
 
-
-  //const deleteQuery = { name: { $in: ["elotes", "fried rice"] } };
-  //try {
-  //  const deleteResult = await collection.deleteMany(deleteQuery);
-  //  console.log(`Deleted ${deleteResult.deletedCount} documents\n`);
-  //} catch (err) {
-  //  console.error(`Something went wrong trying to delete documents: ${err}\n`);
-  //}
-
-  // Make sure to call close() on your client to perform cleanup operations
-  await client.close();
-}
-run().catch(console.dir);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
